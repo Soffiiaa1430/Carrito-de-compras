bdfa61715a73c9008d54a09c4e0e7d7421d1a5c9
sebastian <sebasjimenez1121@gmail.com>
2023-07-13 09:25:24 -0500

    cracion del carrito de compras

---
+++ README.md
@@ -0,0 +1,18 @@
+# Tienda Online
+Este es un proyecto para practicar y profudiza conocimientos en git, utilizando gitflow como metodologia
+
+# Herramientas
+    * Vscode
+    * Github
+    * Git
+
+# Lenguajes
+    * Html
+    * Css
+    * Js
+
+# Metodologia
+    *gitFlow
+
+# Develop by
+    Sofia Cano Aguirre
---
+++ js/contador.js
@@ -0,0 +1,14 @@
+function incrementarContador() {
+    let contador = document.getElementById('contador');
+    contador.innerHTML = parseInt(contador.innerHTML) + 1;
+}
+
+function disminuirContador() {
+    let contador = document.getElementById('contador');
+    let validar = parseInt(contador.innerHTML);
+    if (validar > 0) {
+       contador.innerHTML = validar-1; 
+    }
+    
+}
+
